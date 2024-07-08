4a9bb94e5767bf91d1c5c2baca8296947ca7bbe5
JessicaDabennini <jessica.dabennini@gmail.com>
2024-07-08 10:26:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -48,7 +48,8 @@ const getSearchBarValue = () => document.getElementById('searchBar')?.value.trim
 function fetchSubjectData(categoryValue) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://openlibrary.org/subjects/${categoryValue}.application/json`, true);
+    xhr.open('GET', `https://openlibrary.org/subjects/${categoryValue}.json`, true);
+    xhr.setRequestHeader('Accept', 'application/json');
     xhr.onload = function() {
       if (xhr.status === 200) {
         resolve(JSON.parse(xhr.responseText));
